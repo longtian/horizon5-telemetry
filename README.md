a5dcd2e736ae95a588871a16e5d0f2336533f13b
yan <wyvernnot@gmail.com>
2023-03-29 22:54:23 +0800

    add grafana live

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # horizon5-telemetry
 
+![IMAGE](./img.png)
+
 ### Start
 
 ```
 export PORT=6000
+export THRESHOLD=100
+export URL=ws://10.10.1.104:3000/api/live/push/horizon5
+export TOKEN=*
 npm start 
 ```
 
--- server.js
+++ server.js
@@ -1,28 +1,51 @@
 const udp = require('dgram');
+const WebSocket = require('ws').WebSocket;
 const server = udp.createSocket('udp4');
-
 const parseData = require('./generated_parse_data');
+
 const {
-    port = 6000
+    PORT = 6000,
+    THRESHOLD='100',
+    URL,
+    TOKEN
 } = process.env
+
+let client = null;
+let last_time = Date.now()
+
+if (TOKEN && URL) {
+    client = new WebSocket(URL, {
+        headers: {
+            "Authorization": `Bearer ${TOKEN}`
+        }
+    });
+    client.on('error', console.error);
+    client.on('open', function open() {
+        console.log(`client connected to ${URL}`)
+    });
+}
 
 server.on('message', msg => {
     const payload = parseData(msg)
-    console.clear();
-    console.log(`
-    CarOrdinal: ${payload.CarOrdinal}
-    CarClass: ${payload.CarClass}
-    CarPerformanceIndex: ${payload.CarPerformanceIndex}
-    IsRaceOn: ${payload.IsRaceOn}
-    Speed: ${Math.round(payload.Speed*3.6)} KM/S
-    Accel: ${payload.Accel}
-    Brake: ${payload.Brake}
-    HandBrake: ${payload.HandBrake}
-    Gear: ${payload.Gear}
-    Steer: ${payload.Steer}
-    Height: ${Math.round(payload.PositionY-100)} M
-    NormalizedAIBrakeDifference: ${payload.NormalizedAIBrakeDifference}
-    `);
+    const metrics = [
+        `Speed=${payload.Speed*3.6}`,
+        `Accel=${payload.Accel}`,
+        `Brake=${payload.Brake}`,
+        `HandBrake=${payload.HandBrake}`,
+        `Steer=${payload.Steer}`,
+        `Gear=${payload.Gear}`,
+        `Height=${Math.round(payload.PositionY - 100)}`
+    ]
+    const metric_line = `car_status ${metrics.join(",")} ${Date.now()}000000`
+    if (client && client.readyState === 1) {
+        if (Date.now() - last_time > parseInt(THRESHOLD,10)) {
+            last_time = Date.now()
+            client.send(metric_line)
+        }
+    }
 });
+server.on('listening', () => {
+    console.log(`server listening on port=${PORT}, threshold=${THRESHOLD}`)
+})
 
-server.bind(port);
+server.bind(PORT);
